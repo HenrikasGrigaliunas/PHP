83c770e08242c33cadcb4d629631b39e51f36155
HenrikasGrigaliunas <grigaliunas.h@gmail.com>
2023-01-31 22:29:01 +0200

    update

---
+++ 2023-01-25/homework/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Data transfer PHP</title>
+</head>
+
+<body>
+      <h1>stringų generatorius</h1>
+      <p>Sukurkite failą: save.php, kuriame parašykite programą kuri, toje pačioje direktorijoje,
+sukurtų failą skaičius.txt ir jame išsaugotų atsitiktinį stringą sudarytą iš 3 lotyniškų mažųjų raidžių.
+Sukurkite failą: catch.php, kuriame aprašykite programą kuri generuotų stringą pagal prieš tai buvusią taisyklę ir ją sutikrintų su prieš tai faile išsaugota reikšme. Jei šie du stringai nesutampa veiksmą kartokite tol kol jie abu bus vienodi, o naršyklėje išveskite visus sugeneruotus stringus rezultatui gauti.
+
+Pabandykite apsunkinti užduotį didinant simbolių kiekį, patikrinkite kokį didžiausią kiekį operacijų gali atlikti jūsų kompiuteris.</p>
+
+
+      <div style="margin: 50px auto;">
+            <h2>Nuorodos į failus:</h2>
+            <a href='save.php'>Generate and save new string</a><br />
+            <a href='catch.php'>Try to regenerate string</a></br>
+            <a href='skaicius.txt'>Generated string</a></br>
+      </div>
+</body>
+
+</html>      
